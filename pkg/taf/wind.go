dc89aa52479f7362a85c88d5b1f46a35c006f09c
vadimistar <vadimistar@gmail.com>
2023-08-21 01:32:55 +0700

    implement wind group parsing

---
+++ pkg/taf/wind.go
@@ -0,0 +1,113 @@
+package taf
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func wind(code string) (w *Wind, err error) {
+	defer func() {
+		err = errors.Wrapf(err, "invalid wind group: %s", code)
+	}()
+
+	w = new(Wind)
+
+	if len(code) < 8 {
+		return nil, fmt.Errorf("expect input size to be > 8: %s", code)
+	}
+
+	ddd := code[:3]
+	ff := code[3:5]
+	gg := ""
+	if code[5] == 'G' {
+		gg = code[6:8]
+	}
+	fmfm := code[len(code)-3:]
+	if strings.HasSuffix(code, "KT") {
+		fmfm = "KT"
+	}
+
+	w.Direction, err = direction(ddd)
+	if err != nil {
+		return nil, err
+	}
+
+	w.Speed, err = strconv.Atoi(ff)
+	if err != nil {
+		return nil, fmt.Errorf("invalid wind speed group: %s", ff)
+	}
+	if w.Speed < 0 {
+		return nil, fmt.Errorf("invalid wind speed (negative): %d", w.Speed)
+	}
+
+	if gg != "" {
+		w.Gusts, err = strconv.Atoi(gg)
+		if err != nil {
+			return nil, fmt.Errorf("invalid wind gusts group: %s", gg)
+		}
+		if w.Gusts < 0 {
+			return nil, fmt.Errorf("invalid wind gusts (negative): %d", w.Gusts)
+		}
+	}
+
+	switch fmfm {
+	case "MPS":
+		{
+		}
+	case "KT":
+		w.Speed = int(math.Round(float64(w.Speed) * ktToMps))
+		w.Gusts = int(math.Round(float64(w.Gusts) * ktToMps))
+	case "KMH":
+		w.Speed = int(math.Round(float64(w.Speed) * kmhToMps))
+		w.Gusts = int(math.Round(float64(w.Gusts) * kmhToMps))
+	default:
+		return nil, fmt.Errorf("invalid wind speed/gusts units: %s", fmfm)
+	}
+
+	return w, nil
+}
+
+const ktToMps = 0.51
+const kmhToMps = 0.28
+
+func direction(code string) (dir string, err error) {
+	defer func() {
+		err = errors.Wrapf(err, "invalid wind direction group: %s", code)
+	}()
+
+	if code == "VRB" {
+		return "переменный", nil
+	}
+
+	d, err := strconv.Atoi(code)
+	if err != nil {
+		return "", errors.Wrapf(err, "invalid wind direction: %s", code)
+	}
+
+	switch {
+	case d == 0:
+		return "", nil
+	case (d > 315+23 && d <= 360) || (d > 0 && d <= 23):
+		return "северный", nil
+	case d > 23 && d <= 45+23:
+		return "северо-восточный", nil
+	case d > 45+23 && d <= 90+23:
+		return "восточный", nil
+	case d > 90+23 && d <= 135+23:
+		return "юго-восточный", nil
+	case d > 135+23 && d <= 180+23:
+		return "южный", nil
+	case d > 180+23 && d <= 225+23:
+		return "юго-западный", nil
+	case d > 225+23 && d <= 270+23:
+		return "западный", nil
+	case d > 270+23 && d <= 315+23:
+		return "северо-западный", nil
+	}
+
+	return "", fmt.Errorf("invalid wind direction: %s (%d)", code, d)
+}
---
+++ pkg/taf/wind_test.go
@@ -0,0 +1,129 @@
+package taf
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_direction(t *testing.T) {
+	tests := []struct {
+		code    string
+		wantDir string
+		wantErr bool
+	}{
+		{
+			code:    "",
+			wantErr: true,
+		},
+		{
+			code:    "000",
+			wantDir: "",
+			wantErr: false,
+		},
+		{
+			code:    "-100",
+			wantDir: "",
+			wantErr: true,
+		},
+		{
+			code:    "045",
+			wantDir: "северо-восточный",
+			wantErr: false,
+		},
+		{
+			code:    "360",
+			wantDir: "северный",
+			wantErr: false,
+		},
+		{
+			code:    "VRB",
+			wantDir: "переменный",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.code, func(t *testing.T) {
+			gotDir, err := direction(tt.code)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("direction() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotDir != tt.wantDir {
+				t.Errorf("direction() = %v, want %v", gotDir, tt.wantDir)
+			}
+		})
+	}
+}
+
+func Test_wind(t *testing.T) {
+	tests := []struct {
+		code    string
+		wantW   *Wind
+		wantErr bool
+	}{
+		{
+			code:    "",
+			wantErr: true,
+		},
+		{
+			code:    "VRB01BBVB",
+			wantErr: true,
+		},
+		{
+			code:    "VRB01G14BBVB",
+			wantErr: true,
+		},
+		{
+			code:    "VRB-2MPS",
+			wantErr: true,
+		},
+		{
+			code:    "VRB12G-2MPS",
+			wantErr: true,
+		},
+		{
+			code: "VRB02MPS",
+			wantW: &Wind{
+				Direction: "переменный",
+				Speed:     2,
+				Gusts:     0,
+			},
+		},
+		{
+			code: "VRB02G20KT",
+			wantW: &Wind{
+				Direction: "переменный",
+				Speed:     1,
+				Gusts:     10,
+			},
+		},
+		{
+			code: "VRB02G20MPS",
+			wantW: &Wind{
+				Direction: "переменный",
+				Speed:     2,
+				Gusts:     20,
+			},
+		},
+		{
+			code: "VRB02G20KMH",
+			wantW: &Wind{
+				Direction: "переменный",
+				Speed:     1,
+				Gusts:     6,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.code, func(t *testing.T) {
+			gotW, err := wind(tt.code)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("wind() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotW, tt.wantW) {
+				t.Errorf("wind() = %v, want %v", gotW, tt.wantW)
+			}
+		})
+	}
+}
